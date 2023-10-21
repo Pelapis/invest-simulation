54f5180a450f41cfce16b38ff846b408d29d3f50
Pelapis <cavendish@hnu.edu.cn>
2023-10-21 17:54:49 +0800

    reconstruct the algorithm

--- ror.py
+++ ror.py
@@ -1,123 +1,46 @@
-import os
-import pandas as pd
 import numpy as np
-import scipy.stats as stats
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# Read the data from the CSV file
-path = "~/Desktop/ror/"
-data_index = pd.read_csv(os.path.join(path, "data_index.csv")).iloc[:, 2].values + 1
-data_mao = pd.read_csv(os.path.join(path, "data_mao.csv")).iloc[:, 2].values + 1
-data_meng = pd.read_csv(os.path.join(path, "data_meng.csv")).iloc[:, 2].values + 1
+# Define function to generate RORs
+def gen_RORs(day_return_data, ror_num=100, invest_level=0.5, invest_freq=1, hold=1, trans_cost=0.001):
+    # Get period returns
+    ones = np.ones(int(np.ceil(len(day_return_data) / hold) * hold - len(day_return_data)))
+    period_returns = np.append(day_return_data, ones).reshape(-1, hold).prod(axis=1)
+    # Define function to generate ROR
+    def gen_ROR():
+        # Get focusing periods
+        focusing_periods = np.random.choice((True, False), size=len(period_returns), p=(invest_freq, 1 - invest_freq))
+        # Get if invest or not
+        if_invest = np.random.choice((True, False), size=len(period_returns), p=(invest_level, 1 - invest_level)) == (period_returns > 1)
+        # Get RORs
+        ROR = np.prod(period_returns[focusing_periods & if_invest] * (1 - trans_cost))
+        return ROR
+    return np.array([gen_ROR() for _ in range(ror_num)])
 
-# Choose data
-day_return = data_index
+# Define function to plot ROR line chart with error bars
+def line_with_error_bar(data_name, var_name="Invest Level", x=np.linspace(0.4, 0.6, num=20), fun=lambda x : gen_RORs(day_return_data, invest_level=x)):
+    mean_RORs = np.array([np.mean(fun(x)) for x in x])
+    std_RORs = np.array([np.std(fun(x)) for x in x])
+    k_sd = 0.2
+    plt.figure()
+    plt.plot(x, mean_RORs)
+    plt.errorbar(x, mean_RORs, yerr=k_sd * std_RORs)
+    plt.title(f"Mean ROR vs {var_name} with Error Bars")
+    plt.xlabel(var_name)
+    plt.ylabel("Mean ROR")
+    plt.savefig(f"./pyplots/{data_name}/Mean ROR vs {var_name}.png")
+    plt.close()
 
-# Define investor class
-class Investors:
-    def __init__(self, invest_level=0.5, invest_freq=1, hold=1, num_inv=1000, trans_cost=0.001):
-        """
-        Initialize the investor class with default parameters.
+# Define function to plot different arguments of ROR
+def plot_RORs(data_name="index"):
+    # Import data
+    day_return_data = pd.read_csv(f"./data_{data_name}.csv").iloc[:, 2].values + 1
+    # Plot RORs
+    line_with_error_bar(data_name, var_name="Invest Level", x=np.linspace(0.4, 0.6, num=20), fun=lambda x : gen_RORs(day_return_data, invest_level=x))
+    line_with_error_bar(data_name, var_name="Invest Frequency", x=np.linspace(0, 1, num=20), fun=lambda x : gen_RORs(day_return_data, invest_freq=x))
+    line_with_error_bar(data_name, var_name="Holding Period", x=np.linspace(1, len(day_return_data), num=20, dtype=int), fun=lambda x : gen_RORs(day_return_data, hold=x))
 
-        Parameters:
-        invest_level (float): The investor's investment level.
-        invest_freq (float): The investor's investment frequency.
-        hold (int): The investor's holding period.
-        num_inv (int): The number of investors to simulate.
-        trans_cost (float): The transaction cost to consider.
-        """
-        self.invest_level = invest_level
-        self.invest_freq = invest_freq
-        self.hold = hold
-        self.num_inv = num_inv
-        self.trans_cost = trans_cost
-
-    def rors(self, day_return=day_return):
-        """
-        Calculate the rate of return (ROR) for a given investment strategy.
-
-        Parameters:
-        day_return (numpy.ndarray): The data to use for the ROR calculation.
-
-        Returns:
-        numpy.ndarray: An array of RORs.
-        """
-        # Initialize RORs
-        rors = np.array([])
-
-        # Calculate the number of holding periods
-        num_period = len(day_return) // self.hold
-
-        # Calculate the return of each period
-        period_returns = np.prod(day_return[:num_period * self.hold].reshape(-1, self.hold), axis=1)
-        if len(day_return) % self.hold != 0:
-            period_returns = np.append(period_returns, np.prod(day_return[num_period * self.hold:]))
-
-        # Get the rightful invest strategy
-        right_invest = period_returns > 1
-
-        for i in range(self.num_inv):
-            # Generate consider vector
-            num_consid = round(self.invest_freq * len(period_returns))
-            consider = np.random.binomial(1, self.invest_freq, len(period_returns)).astype(bool)
-
-            # Generate invest vector
-            invest = np.random.binomial(1, self.invest_level, len(period_returns)).astype(bool)
-            invest = invest == right_invest
-
-            invest_return = period_returns[consider & invest]
-
-            ror = np.prod(invest_return)
-
-            # Consider transaction cost
-            ror = (1 - self.trans_cost) ** len(invest_return) * ror
-
-            # Calculate RORs using vectorized operations
-            rors = np.append(rors, ror)
-
-        # Return RORs
-        return rors
-
-long_return = Investors(invest_level = 1, invest_freq = 1, hold = len(day_return), num_inv = 1).rors()[0]
-best_return = Investors(invest_level = 1, invest_freq = 1, hold = 1, num_inv = 1).rors()[0]
-
-
-
-# DENSITY DIAGRAM
-
-# Simulate RORs to plot density diagram
-investor = Investors(invest_level=0.5, invest_freq=1, hold=1, num_inv=10000, trans_cost=0.001)
-rors = investor.rors()
-
-# Calculate data for density diagram
-x = np.linspace(rors.min(), rors.max(), 200)
-y = stats.gaussian_kde(rors)(x)
-
-# Plot density diagram and lines
-plt.figure()
-plt.plot(x, y, color="blue")
-plt.axvline(x=1, color="red")
-plt.axvline(x=long_return, color="green")
-
-# Add legends
-plt.title("Density of ROR")
-plt.xlabel("ROR")
-plt.ylabel("Density")
-plt.legend(
-    [
-        f"Invest Level: {investor.invest_level}\n"
-        f"Invest Frequency: {investor.invest_freq}\n"
-        f"Holding Period: {investor.hold} day(s)\n"
-        f"Number of Investors: {investor.num_inv}\n"
-        f"Mean: {round(np.mean(rors), 4)}",
-        "x = 1",
-        f"Hold for 10 years: x = {round(long_return, 4)}"
-    ],
-    loc="upper right"
-)
-
-# Save the plot to a file
-plt.savefig("~/Desktop/ROR_density.png")
-
-# Close the plot
-plt.close()
+plot_RORs("index")
+plot_RORs("maotai")
+plot_RORs("mengjie")
--- ror.r
+++ ror.r
@@ -1,392 +1,55 @@
 # Import libraries
 library(graphics)
 
-# Import data from csv file
-data_index <- read.csv("~/data_index.csv")[, 3] + 1
-data_mao <- read.csv("~/data_mao.csv")[, 3] + 1
-data_meng <- read.csv("~/data_meng.csv")[, 3] + 1
-
-# Select data to use
-day_return <- data_index
-
 # Define function to generate ROR for a given investment strategy
-gen_rors <- function(day_returns = day_return, num_ror = 1000, invest_level = 0.5, invest_freq = 1, hold = 1, trans_cost = 0.001) {
+gen_RORs <- function(day_returns, num_ror = 100, invest_level = 0.5, invest_freq = 1, hold = 1, trans_cost = 0.001) {
     # Period returns
     gen_period_returns <- function(day_returns = day_returns, hold = hold) {
-        day_returns_add <- c(day_returns, rep(1, times = day_returns %% hold))
+        ones <- rep(1, times = ceiling(length(day_returns) / hold) * hold - length(day_returns))
+        day_returns_add <- c(day_returns, ones)
         day_returns_matrix <- matrix(day_returns_add, nrow = hold)
         period_returns <- apply(day_returns_matrix, 2, prod)
         period_returns
     }
     period_returns <- gen_period_returns(day_returns = day_returns, hold = hold)
 
-    # Focusing periods
-    gen_focusing_periods <- function(returns = period_returns, invest_freq = invest_freq)
-        sample(c(TRUE, FALSE), size = length(returns), replace = TRUE, prob = c(invest_freq, 1 - invest_freq))
-
     # RORs
     RORs <- replicate(num_ror, {
-        focusing_periods <- gen_focusing_periods(returns = period_returns, invest_freq = invest_freq)
+        # Focusing periods
+        focusing_periods <- sample(c(TRUE, FALSE), size = length(period_returns), replace = TRUE, prob = c(invest_freq, 1 - invest_freq))
+        # Investment choices
         if_invest <- sample(c(TRUE, FALSE), size = length(period_returns), replace = TRUE, prob = c(invest_level, 1 - invest_level)) == (period_returns > 1)
-        prod((period_returns * (1 - trans_cost))[focusing_periods & if_invest])
+        # ROR
+        prod(period_returns[focusing_periods & if_invest] * (1 - trans_cost))
     })
     RORs
 }
 
+# ROR line chart with error bars
+line_with_error_bar <- function(data_name, x = seq(0.4, 0.6, length.out = 20), var = "Invest Level", FUN = function(x) gen_RORs(invest_level = x, day_returns = day_return_data)) {
+    mean_RORs <- sapply(x, function(x) mean(FUN(x)))
+    sd_RORs <- sapply(x, function(x) sd(FUN(x)))
+    k_sd <- 0.12
+    y_lower <- mean_RORs - k_sd * sd_RORs
+    y_upper <- mean_RORs + k_sd * sd_RORs
 
-
-# BOXPLOT OF ROR WITH DIFFERENT INVEST LEVELS
-
-# Set the max number, invest_freq and invest_level
-num_ror <- 1000
-invest_freq <- 1
-
-# Simulate RORs with different invest levels, and plot the boxplot diagram
-png("./plots/不同投资水平的ROR箱线图.png")
-boxplot(
-    gen_rors(day_return, num_ror, 0.475, invest_freq),
-    gen_rors(day_return, num_ror, 0.5, invest_freq),
-    gen_rors(day_return, num_ror, 0.525, invest_freq),
-    gen_rors(day_return, num_ror, 0.55, invest_freq),
-    gen_rors(day_return, num_ror, 0.56, invest_freq),
-    gen_rors(day_return, num_ror, 0.575, invest_freq),
-    main = "Boxplot of ROR with different invest levels",
-    xlab = "Invest Level",
-    ylab = "ROR",
-    names = c("0.475", "0.5", "0.525", "0.55", "0.56", "0.575")
-    )
-# Add a y = 1.5346 line
-abline(h = 1.5346, col = "red")
-# Add a legend of num_ror, invest level and the line
-legend(
-    "topleft",
-    legend = c(
-        paste("Numbers:", num_ror),
-        paste("Invest frequency:", invest_freq),
-        paste("Hold for 10 years ROR = 1.5346")
-    ),
-    col = c("black", "black", "red"),
-    lty = c(1, 1, 1), cex = 0.8
-)
-dev.off()
-
-
-
-# CURVE OF MEAN ROR VS. INVEST LEVEL
-
-# Set invest levels and standard deviation of RORs
-invest_levels <- seq(0, 1, by = 0.01)
-mean_rors <- sapply(
-    invest_levels,
-    function(x) mean(gen_rors(num_ror = 100, invest_level = x))
-)
-
-# Plot the mean RORs against the invest levels
-png("./plots/不同投资水平的ROR均值曲线.png")
-plot(
-    invest_levels,
-    mean_rors,
-    type = "l",
-    xlab = "Invest Level",
-    ylab = "Mean ROR",
-    main = "Mean ROR vs. Invest Level"
-)
-dev.off()
-
-# Plot the log of mean RORs against the invest levels
-png("./plots/不同投资水平的ROR均值曲线_log.png")
-plot(
-    invest_levels,
-    log(mean_rors),
-    type = "l",
-    xlab = "Invest Level",
-    ylab = "Log Mean ROR",
-    main = "Log Mean ROR vs. Invest Level"
-)
-dev.off()
-
-
-
-# CURVE OF STANDARD DEVIATION VS. INVEST LEVEL
-
-# Set invest levels and standard deviation of RORs
-invest_levels <- seq(0, 1, by = 0.01)
-sd_rors <- sapply(
-    invest_levels,
-    function(x) sd(gen_rors(num_ror = 100, invest_level = x))
-)
-
-# Plot the standard deviation of RORs against the invest levels
-png("./plots/不同投资水平的ROR标准差曲线.png")
-plot(
-    invest_levels,
-    sd_rors,
-    type = "l",
-    xlab = "Invest Level",
-    ylab = "Standard Deviation of ROR",
-    main = "Standard Deviation of ROR vs. Invest Level"
-)
-dev.off()
-
-
-
-# BOXPLOT OF ROR WITH DIFFERENT FREQUENCIES
-
-# Set the max number, invest_freq and invest_level
-num_ror <- 1000
-invest_level <- 0.56
-
-# Simulate RORs with different frequencies, and plot the boxplot diagram
-png("./plots/不同频率的ROR箱线图.png")
-boxplot(
-    gen_rors(day_return, num_ror, invest_level, 0,),
-    gen_rors(day_return, num_ror, invest_level, 0.25),
-    gen_rors(day_return, num_ror, invest_level, 0.5),
-    gen_rors(day_return, num_ror, invest_level, 0.75),
-    gen_rors(day_return, num_ror, invest_level, 1),
-    main = "Boxplot of ROR with different frequencies",
-    xlab = "Frequency",
-    ylab = "ROR",
-    names = c("0", "0.25", "0.5", "0.75", "1")
-    )
-# Add a y = 1.5346 line
-abline(h = 1.5346, col = "red")
-# Add a legend of num_ror, invest level and the line
-legend(
-    "topleft",
-    legend = c(
-        paste("Numbers:", num_ror),
-        paste("Invest level:", invest_level),
-        paste("Hold for 10 years ROR = 1.5346")
-    ),
-    col = c("black", "black", "red"),
-    lty = c(1, 1, 1), cex = 0.8
-)
-dev.off()
-
-
-
-# CURVE OF STANDARD DEVIATION VS. INVEST FREQUENCY
-
-# Set investment frequencies and standard deviations of RORs
-invest_freqs <- seq(0, 1, by = 0.01)
-sd_rors <- sapply(
-    invest_freqs,
-    function(x) sd(gen_rors(num_ror = 100, invest_freq = x))
-)
-
-# Plot the standard deviation of RORs against the invest frequencies
-png("./plots/不同投资频率的ROR标准差曲线.png")
-plot(
-    invest_freqs,
-    sd_rors,
-    type = "l",
-    xlab = "Invest Frequency",
-    ylab = "Standard Deviation of ROR",
-    main = "Standard Deviation of ROR vs. Invest Frequency"
-)
-dev.off()
-
-
-
-# 错过涨幅最大的几天
-
-# Calculate ROR for a given number of missed days
-calculate_ror <- function(missed_days, day_return) {
-  ordered_day_return <- day_return[order(day_return, decreasing = TRUE)]
-  prod(ordered_day_return[(missed_days + 1):length(ordered_day_return)])
+    png(paste0("./rplots/", data_name, "/Mean ROR vs ", var, ".png"))
+    plot(x = x, y = mean_RORs, type = "l", main = paste0("Mean ROR vs ", var), xlab = var, ylab = "Mean ROR")
+    sapply(seq_along(x), function(i)
+        segments(x0 = x[i], y0 = y_lower[i], y1 = y_upper[i], lwd = 2))
+    dev.off()
 }
 
-# Define missed days
-missed_days <- c(0, 5, 10, 20, 25, 30, 40, 50)
-
-# Calculate ROR for each missed day
-ror <- sapply(missed_days, calculate_ror, day_return = day_return)
-
-# Print ROR values
-cat(paste("ROR for missed", missed_days, "days:", ror, "\n"))
-
-# Plot the bar chart with x-axis labels
-png("./plots/错过涨幅最大的几天条形图.png")
-barplot(
-  ror,
-  main = "Relationship between ROR and Missed Days",
-  xlab = "Missed Days",
-  names.arg = missed_days
-)
-dev.off()
-
-# Plot the line diagram
-png("./plots/错过涨幅最大的几天折线图.png")
-plot(
-  missed_days,
-  ror,
-  type = "l",
-  xlab = "Missed Days",
-  ylab = "ROR",
-  main = "Relationship between ROR and Missed Days"
-)
-dev.off()
-
-
-
-# 避开跌幅最大的几天
-
-# Calculate ROR for a given number of missed days
-calculate_ror <- function(missed_days, day_return) {
-  ordered_day_return <- day_return[order(day_return, decreasing = FALSE)]
-  prod(ordered_day_return[(missed_days + 1):length(ordered_day_return)])
+# Define function to generate plots
+main_plot <- function(data_name = "index") {
+    # Import data from csv file
+    day_return_data <- read.csv(paste0("./data_", data_name, ".csv"))[, 3] + 1
+    # Plot line chart with error bars
+    line_with_error_bar(data_name = data_name, x = seq(0.4, 0.6, length.out = 20), var = "Invest Level", FUN = function(x) gen_RORs(invest_level = x, day_returns = day_return_data))
+    line_with_error_bar(data_name = data_name, x = seq(0, 1, length.out = 20), var = "Invest Freqency", FUN = function(x) gen_RORs(invest_freq = x, day_returns = day_return_data))
+    line_with_error_bar(data_name = data_name, x = seq(1, length(day_return_data), length.out = 20), var = "Hold Period", FUN = function(x) gen_RORs(hold = x, day_returns = day_return_data))
 }
 
-# Define missed days
-missed_days <- c(0, 5, 10, 20, 25, 30, 40, 50)
-
-# Calculate ROR for each missed day
-ror <- sapply(missed_days, calculate_ror, day_return = day_return)
-
-# Print ROR values
-cat(paste("ROR for missed", missed_days, "days:", ror, "\n"))
-
-# Plot the bar chart with x-axis labels
-png("./plots/避开跌幅最大的几天条形图.png")
-barplot(
-  ror,
-  main = "Relationship between ROR and Missed Days",
-  xlab = "Missed Days",
-  names.arg = missed_days
-)
-dev.off()
-
-# Plot the line diagram
-png("./plots/避开跌幅最大的几天折线图.png")
-plot(
-  missed_days,
-  ror,
-  type = "l",
-  xlab = "Missed Days",
-  ylab = "ROR",
-  main = "Relationship between ROR and Missed Days"
-)
-dev.off()
-
-
-
-# 避开跌幅最大的几天条形图
-
-# Define x-axis labels
-x_labels <- paste("Missed", missed_days, "Days", sep = " ")
-
-# Plot the bar chart with x-axis labels
-png("./plots/避开跌幅最大的几天条形图.png")
-barplot(
-  ror,
-  main = "Relationship between ROR and Missed Days",
-  xlab = "Missed Days",
-  names.arg = x_labels
-)
-dev.off()
-
-
-
-# BOXPLOT OF ROR FOR DIFFERENT HOLDING PERIODS
-
-# Set coordinates
-holds <- c(1, 5, 22, 63, 250, 500, 2430)
-xy <- lapply(holds, function(x) gen_rors(hold = x))
-xy <- setNames(xy, holds)
-
-# Plot boxplot of RORs for different holding periods
-png("./plots/不同持有期的ROR箱线图.png")
-boxplot(
-    xy,
-    main = "ROR Boxplot for Different Hold Periods",
-    xlab = "Hold Period",
-    ylab = "ROR"
-)
-dev.off()
-
-
-
-# CURVE OF MEAN ROR VS. HOLDING PERIOD
-
-# Generate holds and mean of RORs
-holds <- seq(1, 2430, by = 10)
-mean_rors <- sapply(
-    holds,
-    function(hold) mean(gen_rors(num_ror = 100, hold = hold))
-)
-
-# Plot the mean of RORs against the holding period
-png("./plots/不同持有期的ROR均值曲线.png")
-plot(
-    holds,
-    mean_rors,
-    type = "l",
-    xlab = "Hold Period",
-    ylab = "Mean ROR",
-    main = "Mean ROR vs. Hold Period"
-)
-dev.off()
-
-
-
-# CURVE OF STANDARD DEVIATION VS. HOLD PERIOD
-
-# Set holds and standard deviation of RORs
-holds <- seq(1, 2430, by = 10)
-sd_rors <- sapply(
-    holds,
-    function(hold) sd(gen_rors(num_ror = 100, hold = hold))
-)
-
-# Plot the standard deviation of RORs against the invest frequencies
-png("./plots/不同持有期的ROR标准差曲线.png")
-plot(
-    holds,
-    sd_rors,
-    type = "l",
-    xlab = "Hold Period",
-    ylab = "Standard Deviation of ROR",
-    main = "Standard Deviation of ROR vs. Hold Period"
-)
-dev.off()
-
-
-
-# DENSITY OF ROR
-
-# Prepare data
-num_ror <- 1000
-invest_level <- 0.5
-invest_freq <- 1
-hold <- 1
-rors <- gen_rors(
-    num_ror = num_ror,
-    invest_level = invest_level,
-    invest_freq = invest_freq,
-    hold = hold
-)
-mean_rors <- round(mean(rors), 4)
-xy <- density(rors)
-
-# Plot the density curve
-png("./plots/ROR的概率密度图.png")
-plot(xy, main = "Density of ROR", xlab = "ROR")
-
-# Add x = 1 line
-abline(v = 1, col = "red")
-
-# Add legend
-legend(
-    "topright",
-    legend = c(
-        paste("numbers:", num_ror),
-        paste("invest level:", invest_level),
-        paste("invest freqency:", invest_freq),
-        paste("hold:", hold),
-        paste("mean:", mean_rors)
-    )
-)
-
-# Close the graphics device
-dev.off()
+main_plot("index")
+main_plot("maotai")
+main_plot("mengjie")

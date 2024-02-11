a7123a71d8a63646e4c6a1e2d5799464da3fe586
Flightless <cavendish@hnu.edu.cn>
2024-02-12 00:12:01 +0800

    update

--- src/rust/src/bin/test.rs
+++ src/rust/src/bin/test.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let a = ["1", "two", "NaN", "four", "5"];
-    let mut iter = a
-        .iter()
-        .map(|s| s.parse())
-        .filter(|s| s.is_ok())
-        .map(|s| s.ok());
-    assert_eq!(iter.next(), Some(Some((1))));
-    assert_eq!(iter.next(), Some(Some(5)));
-    assert_eq!(iter.next(), None);
+    let a = vec![1, 2, 3];
+    let b = &[4, 5, 6][..];
+
+    let c = a.iter().chain(b.iter());
 }
